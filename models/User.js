fde060907051f566669cbbd8326269d5bd3af5f5
Dawn LeBlanc <dawnleblanc11@gmail.com>
2022-01-24 13:41:56 -0500

    user model setup

---
+++ models/User.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      match: [/.+\@.+\..+/]
+    },
+    thoughts: {
+      type: Schema.Types.ObjectId,
+      ref: 'Thought' // referencing Thought doc model
+    },
+    friends: [
+        {
+      type: Schema.Types.ObjectId,
+      ref: 'User' // referencing user doc model
+    }
+],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    // prevents virtuals from creating duplicate of _id as `id`
+    id: false
+  }
+);
+
+// get total count of friends on retrieval
+UserSchema.virtual('friendCount').get(function() {
+  return this.friends.length;
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
